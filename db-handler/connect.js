29820f9df77aecf29d74fef3dfa057c4aed73844
Jeremy Penaredondo <jay.pena1224@proton.me>
2026-05-01 16:23:21 -0400

    Creates MongoDB connection script

---
+++ db-handler/connect.js
@@ -0,0 +1,19 @@
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+dotenv.config();
+const uri = process.env.MONGO_URL;
+if (!uri) {
+    throw new Error("MongoDB connection URI is not defined in environment variables.");
+}
+const client = new MongoClient(uri);
+const connectToDatabase = async () => {
+    try {
+        await client.connect();
+        console.log("Connected to MongoDB");
+    }
+    catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+        throw error;
+    }
+};
+connectToDatabase();
